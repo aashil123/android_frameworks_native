519e29b94a34f04a24b178d7c5d88a53f67552ff
aashil123 <singhalaashil@gmail.com>
2020-07-15 10:04:03 +0000

    Revert "Merge tag 'LA.UM.8.2.r1-06700-sdm660.0' of https://source.codeaurora.org/quic/la/platform/frameworks/native into HEAD"
    This reverts commit 9185e2137ca711d22f6c1e01d8a693e842125857, reversing
    changes made to 7942ad96f0e6df24c65d1c12e3f2c676a9460e04.

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -1513,7 +1513,7 @@ status_t SurfaceFlinger::setDisplayElapseTime(const sp<DisplayDevice>& display) 
         return OK;
     }
 
-    if (mDisplaysList.size() != 1 || display->isVirtual()) {
+    if (mDisplaysList.size() != 1) {
         // Revisit this for multi displays.
         return OK;
     }
@@ -4334,12 +4334,6 @@ bool SurfaceFlinger::requiresProtecedContext(const sp<DisplayDevice>& displayDev
     auto& renderEngine = getRenderEngine();
     auto display = displayDevice->getCompositionDisplay();
     if (displayDevice->getId()) {
-        // For display sinks which are not secure, avoid protected
-        // content support in SurfaceFlinger
-        if (!display->isSecure()) {
-            return false;
-        }
-
         for (auto& layer : displayDevice->getVisibleLayersSortedByZ()) {
             // If the layer is a protected layer, mark protected context is needed.
             if (layer->isProtected()) {
